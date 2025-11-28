632bc63e4a154e602c00e93f36b5a43d97746f77
k0dekk <sashaoksiuk@gmail.com>
2025-11-28 15:57:35 +0200

    Update 3-wrapper.js

--- Lab8/3-wrapper.js
+++ Lab8/3-wrapper.js
@@ -1,45 +1,38 @@
 'use strict'
 
-const contract = (fn, ...types) => (...args) => {
-  for (let i = 0; i < args.length; i++) {
-    const arg = args[i];
-    const def = types[i];
-    const name = def.name.toLowerCase();
-    if (typeof arg !== name) {
-      throw new TypeError(`Argument type ${name} expected`);
+function contract(fn, ...types) {
+  return function (...args) {
+
+    for (let i = 0; i < args.length; i++) {
+      const expected = types[i];
+      const actual = args[i];
+
+      if (typeof actual !== expected.name.toLowerCase()) {
+        throw new TypeError(
+          `Arg ${i + 1}  ${expected.name}`
+        );
+      }
     }
-  }
-  const res = fn(...args);
-  const def = types[types.length - 1];
-  const name = def.name.toLowerCase();
-  if (typeof res !== name) {
-    throw new TypeError(`Result type ${name} expected`);
-  }
-  return res;
-};
+
+    const result = fn(...args);
+
+    const expectedReturn = types[types.length - 1];
+    if (typeof result !== expectedReturn.name.toLowerCase()) {
+      throw new TypeError(
+        `expected typeof res ${expectedReturn.name}`
+      );
+    }
+
+    return result;
+  };
+}
 
 const add = (a, b) => a + b;
 const addNumbers = contract(add, Number, Number, Number);
-
-try {
-  const res = addNumbers(2, 3);
-  console.log(`addNumbers(2, 3) = ${res}`);
-} catch (e) {
-  console.error(e.message);
-}
+const res1 = addNumbers(2, 3);
+console.dir(res1); // Output: 5
 
 const concat = (s1, s2) => s1 + s2;
 const concatStrings = contract(concat, String, String, String);
-
-try {
-  const res = concatStrings('Hello ', 'world!');
-  console.log(`concatStrings result: '${res}'`);
-} catch (e) {
-  console.error(e.message);
-}
-
-try {
-  addNumbers(2, '3');
-} catch (e) {
-  console.error("Помилка (очікувана):", e.message); 
-}
+const res2 = concatStrings('Hello ', 'world!');
+console.dir(res2); // Output: Hello world!

68455429535e0832b961b79291c8036905d997ad
k0dekk <sashaoksiuk@gmail.com>
2026-04-26 00:06:42 +0300

    implement callback and promise versions of asyncSome

---
+++ 2sem/lab5/asyncSome.js
@@ -0,0 +1,20 @@
+export function asyncSomeCallback(arr, predicate, cb) {
+  function next(i) {
+    if (i >= arr.length) return cb(null, false);
+
+    predicate(arr[i], i, arr, (err, res) => {
+      if (err) return cb(err);
+      if (res) return cb(null, true);
+      next(i + 1);
+    });
+  }
+  next(0);
+}
+
+export async function asyncSomePromise(arr, predicate) {
+  for (let i = 0; i < arr.length; i++) {
+    const res = await predicate(arr[i], i, arr);
+    if (res) return true;
+  }
+  return false;
+}

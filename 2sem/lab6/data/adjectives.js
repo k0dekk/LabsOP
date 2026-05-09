46fe0e99c1dcd9e0efe68639a82e6cb39bb1cd1c
k0dekk <sashaoksiuk@gmail.com>
2026-05-09 23:45:30 +0300

    init structure and add dictionaries for generation

---
+++ 2sem/lab6/data/adjectives.js
@@ -0,0 +1,42 @@
+const ADJECTIVES = [
+  'Agile','Ambitious','Ancient','Angry','Arcane','Arctic','Astral','Atomic','Audacious','Azure',
+
+  'Blazing','Bleak','Blind','Bold','Brave','Brilliant','Broken','Bronze','Brutal','Burning',
+
+  'Calm','Celestial','Chaotic','Chrome','Clever','Colossal','Cosmic','Crafty','Crimson','Crystal',
+
+  'Daring','Dark','Deadly','Deep','Dense','Devious','Digital','Dire','Divine','Dominant',
+
+  'Eager','Ebony','Elusive','Ember','Emerald','Endless','Epic','Eternal','Ethereal','Exotic',
+
+  'Fabled','Fallen','Fearless','Ferocious','Fierce','Flaming','Fleet','Fluid','Forbidden','Frozen',
+
+  'Galactic','Ghostly','Giant','Gilded','Glacial','Glorious','Golden','Grand','Grim','Grizzled',
+
+  'Hallowed','Haunted','Heroic','Hidden','Hollow','Holy','Howling','Humble','Hungry','Hyper',
+
+  'Icy','Immortal','Imperial','Infernal','Infinite','Invincible','Iron','Ivory','Jade','Jagged',
+
+  'Keen','Kinetic','Legendary','Lethal','Lightning','Limitless','Lone','Lost','Loyal','Luminous',
+
+  'Majestic','Malevolent','Marble','Midnight','Mighty','Molten','Mystic','Neon','Nimble','Noble',
+
+  'Obsidian','Ominous','Onyx','Optimal','Orbital','Outer','Outlaw','Phantom','Primal','Prismatic',
+
+  'Radiant','Rapid','Raven','Relentless','Remote','Rogue','Rugged','Ruined','Ruthless','Sacred',
+
+  'Savage','Scarlet','Secret','Serene','Shadow','Shattered','Silent','Silver','Sinister','Skeletal',
+  'Solar','Solemn','Spectral','Speedy','Stellar','Stone','Storm','Supreme','Swift','Synthetic',
+
+  'Tainted','Tempest','Terrible','Thunder','Titan','Toxic','Turbulent','Twin','Twisted','Tactical',
+
+  'Ultra','Undying','Unknown','Unstable','Untamed','Valiant','Vast','Vengeful','Venomous','Vibrant',
+
+  'Vicious','Vigilant','Violent','Viral','Volcanic','Wandering','Warped','Wicked','Wild','Wise',
+
+  'Withered','Wrathful','Xenon','Yearning','Zealous','Zenith','Zero','Binary','Cyber','Delta',
+
+  'Quantum','Flux','Grid','Prism','Ion','Echo','Mythic','Raging','Hollow','Blazing',
+];
+
+module.exports = { ADJECTIVES };
---
+++ 2sem/lab6/data/nouns.js
@@ -0,0 +1,31 @@
+const NOUNS = [
+  // Animals
+  'Wolf','Bear','Eagle','Tiger','Lion','Fox','Hawk','Raven','Viper','Shark',
+  'Falcon','Panther','Dragon','Griffin','Phoenix','Hydra','Cobra','Jaguar','Lynx','Osprey',
+  // Nature
+  'Storm','Thunder','Lightning','Blizzard','Tornado','Avalanche','Glacier','Volcano','Comet','Meteor',
+  'Tempest','Typhoon','Cyclone','Tsunami','Quake','Frost','Flame','Ember','Ash','Tide',
+  // Warriors
+  'Knight','Warrior','Hunter','Archer','Mage','Rogue','Paladin','Ranger','Scout','Sentinel',
+  'Guardian','Champion','Crusader','Warden','Phantom','Reaper','Slayer','Vanguard','Titan','Specter',
+  // Weapons
+  'Blade','Shield','Arrow','Hammer','Spear','Axe','Dagger','Staff','Sword','Lance',
+  'Cannon','Bolt','Fist','Claw','Horn','Fang','Crown','Helm','Rune','Seal',
+  // Space
+  'Pulsar','Nebula','Quasar','Orbit','Nova','Singularity','Matrix','Vector','Cipher','Nexus',
+  'Node','Core','Grid','Signal','Pulse','Byte','Flux','Drone','Satellite','Reactor',
+  // Abstract
+  'Shadow','Spirit','Soul','Fury','Rage','Wrath','Glory','Honor','Valor','Legend',
+  'Myth','Omen','Chaos','Order','Fate','Void','Abyss','Destiny','Oath','Vow',
+  // Terrain
+  'Mountain','Canyon','Tundra','Desert','Forest','Swamp','Reef','Crater','Ridge','Peak',
+  'Vale','Cliff','Summit','Hollow','Cave','Rift','Shore','Dune','Mesa','Gorge',
+  // Mythical
+  'Golem','Demon','Angel','Wraith','Banshee','Cyclops','Chimera','Basilisk','Kraken','Leviathan',
+  'Cerberus','Manticore','Wyvern','Sprite','Revenant','Lich','Behemoth','Gargoyle','Djinn','Colossus',
+  // People
+  'Pirate','Ninja','Samurai','Viking','Gladiator','Spartan','Oracle','Prophet','Sage','Elder',
+  'Exile','Outlaw','Rebel','Nomad','Wanderer','Pilgrim','Warlord','Emperor','Tyrant','Sovereign',
+];
+
+module.exports = { NOUNS };
